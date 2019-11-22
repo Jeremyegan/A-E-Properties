e466e785a19b1e5741ef99ae488e95ba69f27cfe
Jeremyegan <jeremyegan1@gmail.com>
2019-11-21 21:50:12 -0700

    feat: Search folders and files, routing

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from './Components/Home/Home';
+import Search from './Components/Search/Search';
 
 
 export default(
     <div>
         <Switch>
             <Route exact path="/" component={Home} />
+            <Route path="/search" component={Search} />
 
         </Switch>
     </div>
